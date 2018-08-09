ced46363c71fa4254ad7f95cd80ee4bbf50a91b3
VolhaBarysava <Olga_Borisova_V@mail.ru>
2018-08-09 19:24:25 +0300

    Update MailCreationTest.java

--- module8/src/test/java/tests/MailCreationTest.java
+++ module8/src/test/java/tests/MailCreationTest.java
@@ -16,8 +16,10 @@ public class MailCreationTest extends TestBase {
 	@Test(description = "Login to mail.ru", groups={"test_1"})
 	public void mailRuLoginTest() {
 		AccountPage accountPage = emailService.loginToMailRu(firstUser);
+// 		please see my comment to the loginToMailRu method in service. There is no need to have a page in a test, please remove it
 		ScreenShooter.takeScreenshot();
 		Assert.assertTrue(accountPage.isEmailPresentOnPage(EXPECTED_ACCOUNT_ICON), "User is not login!");
+// 		a service method should be created for accountPage.isEmailPresentOnPage
 	}
 	
 	@Test(description = "Mail creation", dependsOnMethods = { "mailRuLoginTest" }, groups={"test_1"})
@@ -30,9 +32,12 @@ public class MailCreationTest extends TestBase {
 		mailCreationPage.fillMailSubject(email.getSubject());
 		mailCreationPage.fillMailBody(email.getTextBody());
 		mailCreationPage.clickSaveDraftBtn();
+// 		all the actions above can be combined in one service method: createDraftEmail
 		
 		DraftPage draftPage = accountPage.clickMailDraftMenuLink();
 		String actualSubject = draftPage.getDraftMailSubject(EMAIL_INDEX_NUMBER);
+// 		two actions above can be combined in one service method: getDraftEmailSubject
+// 		please follow the same approach to the rest of page methods in all tests
 		Assert.assertEquals(actualSubject,email.getSubject());
 		
 		draftPage.openDraftMail(0);
